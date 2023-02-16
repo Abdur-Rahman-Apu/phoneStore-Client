8884c105cd2d8a2bc5afa8462e990b8d812c070b
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-16 17:12:32 +0600

    Main layout is created

---
+++ src/Pages/Shared/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <div>404ERROR</div>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/Routes/Routes.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main";
+import Home from "../Pages/Home/Home";
+import Error from "../Pages/Shared/Error";
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <Error></Error>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
