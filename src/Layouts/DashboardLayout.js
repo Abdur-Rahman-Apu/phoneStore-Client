ccccdf0300d1aaca8fd439abe4f7069d673cc6a0
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-20 23:05:58 +0600

    Category reload problem is solved

--- src/Layouts/DashboardLayout.js
+++ src/Layouts/DashboardLayout.js
@@ -3,6 +3,8 @@ import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../Context/AuthProvider';
 import useRole from '../CustomHooks/useRole';
 import Navbar from '../Pages/Shared/Navbar';
+import Lottie from "lottie-react";
+import Load from '../assets/load.json'
 
 const DashboardLayout = () => {
     const { user, loading } = useContext(AuthContext)
@@ -35,7 +37,10 @@ const DashboardLayout = () => {
     }
 
     if (loading) {
-        return <><div>Loading...</div></>
+        return <div className='h-[500px] w-[500px] mx-auto'>
+            <Lottie animationData={Load} loop={true} />
+            <p className='text-center text-3xl font-bold text-sky-400'>Wait loading...</p>
+        </div>
     }
     return (
         <div>
--- src/Layouts/Main.js
+++ src/Layouts/Main.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from '../Pages/Shared/Navbar';
 import { Outlet } from 'react-router-dom'
 import Footer from '../Pages/Shared/Footer';
+import { AuthContext } from '../Context/AuthProvider';
+import Lottie from "lottie-react";
+import Load from '../assets/load.json'
 
 const Main = () => {
+    const { loading } = useContext(AuthContext)
+
+    if (loading) {
+        return <div className='h-[500px] w-[500px] mx-auto'>
+            <Lottie animationData={Load} loop={true} />
+            <p className='text-center text-3xl font-bold text-sky-400'>Wait loading...</p>
+        </div>
+    }
     return (
         <div>
             <Navbar></Navbar>
