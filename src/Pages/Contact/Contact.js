e41581b29243395074b91a0c9978a99fb054f986
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-21 07:43:09 +0600

    Contact us page functionality is added

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Lottie from "lottie-react";
 import ContactAnimation from '../../assets/contact.json';
+import emailjs from '@emailjs/browser';
+import { toast } from 'react-hot-toast';
 
 const Contact = () => {
+    const form = useRef();
+
+    const handleContact = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_vj9xcfu', 'template_rk1igog', form.current, 'huB7HdbtLQHtlBqmr')
+            .then((result) => {
+                toast.success(result.text, {
+                    duration: 4000,
+                    position: 'top-center'
+                });
+            }, (error) => {
+                toast.error(error.text, {
+                    duration: 4000,
+                    position: 'top-center'
+                })
+            });
+        e.target.reset()
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -13,7 +34,7 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form ref={form} onSubmit={handleContact} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text font-bold text-black">Name</span>
@@ -42,7 +63,7 @@ const Contact = () => {
                         <div className="form-control mt-6">
                             <input className='btn border-0 bg-boldGreen hover:bg-green-700 ' type="submit" value="Submit" />
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
